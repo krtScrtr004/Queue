0f04a1286b3a0f4d11bb3870d3c34bae507ca452
Kurt Secretario <secretariokurt3219@gmail.com>
2024-07-24 13:54:35 +0800

    add isFull and isEmpty operations

--- Queue/Queue.h
+++ Queue/Queue.h
@@ -8,15 +8,37 @@
 namespace Queue
 {
 	template <typename Type>
-	class Queue
+	class queue
 	{
 	protected:
 		virtual void enqueue(const Type&) = 0;
 		virtual void dequeue() = 0;
-		virtual Type first() = 0;
-		virtual Type back() = 0;
-		virtual bool isFull();
-		virtual bool isEmpty();
+		virtual Type first() const = 0;
+		virtual Type back() const = 0;
+		virtual bool isFull() const = 0;
+		virtual bool isEmpty() const = 0;
+	};
+
+	template <typename Type>
+	class queueArray
+	{
+	private:
+		constexpr std::size_t CAPACITY;
+		std::size_t size;
+		std::size_t frontIndex;
+		std::size_t backIndex;
+
+		std::array<Type, CAPACITY> container;
+
+	public:
+		void enqueue(const  Type& data) override
+		{
+			// if ()
+		}
+
+		bool isFull() const override { return (size == CAPACITY); }
+
+		bool isEmpty() const override { return (size == 0); }
 	};
 }
 
